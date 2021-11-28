4ab1e548f61344fa6e9b4d24779c0ae493818f5d
FS <80700012+imFS@users.noreply.github.com>
2021-11-28 02:39:15 +0200

    Update main.js
    // TODOS: Code cleanup/optimization

--- main.js
+++ main.js
@@ -1,22 +1,59 @@
-var mangascraper = require("./iscr");
+var img_scraper = require("./iscr");
+var fs = require("fs");
 
-let savePath = __dirname + "\\elite_class_manga\\";
-let pageDelay = 1;
+// Vars~
+let homePath = "elite_class_manga";
+let savePath = __dirname + "\\" + homePath + "\\";
+let pageDelay = 5;
 
-// TODOS: Change 'pages' option below and maybe use 'split' to split by '-' to avoid double digit problem because current method takes single char/last char only.
+// TODOS: Code cleanup/optimization
 
 async function sleep(i) {
   await new Promise((r) => setTimeout(r, i * 1000));
 }
-// Main
-// Download all chapters from 1 to 14 of 'Classroom of the elite'
-async function main() {
-  for (let index = 1; index < 15; index++) {
+
+// Generates an homepage for chapter overview
+function homeGen(start, end) {
+  let chapterPath = "classroom-of-the-elite-chapter";
+
+  let html =
+    "<!DOCTYPE html><head><title>" + homePath + "</title></head><body>";
+  for (let index = start; index < end; index++) {
+    html +=
+      "<a href='./" +
+      chapterPath +
+      "-" +
+      index +
+      "/index.htm'>Chapter " +
+      index +
+      "</a></br>";
+  }
+  html += "</body></html>";
+
+  var indexpath = savePath + "index.htm";
+  fs.writeFile(indexpath, html, function (err) {
+    if (err) console.log("GenFile write error: ", err);
+  });
+}
+
+// Downloads chapters
+async function downloadChapters(start, end) {
+  for (let index = start; index < end; index++) {
     var url = `https://classroomoftheelite.com/manga/classroom-of-the-elite-chapter-${index}/`;
     //console.log(url);
-    mangascraper.scrape(url, savePath);
-    await sleep(pageDelay);
+    img_scraper.scrape(url, savePath);
+
+    if (index % 3 == 0) await sleep(pageDelay);
   }
+}
+
+// Main
+async function main() {
+  let start = 1;
+  let end = 49;
+
+  downloadChapters(start, end);
+  homeGen(start, end);
 }
 
 main();
